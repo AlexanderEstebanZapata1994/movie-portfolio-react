f03307ad7cbaeb276e67f88658efca6e5936d0c8
Alexander Esteban Zapata <AlexanderEstebanZapata1994@users.noreply.github.com>
2026-06-24 18:19:13 -0500

    feat: Add more info to footer section

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ function App() {
       <h1 className="text-4xl text-white py-10 text-center font-mono">My Movies Portfolio</h1>
       <MovieGrid />
       <footer className="text-center text-white py-10">
-        <p>Powered by TMDB, this is a no-profit project for learning purposes</p>
+        <p className="text-sm">Powered by TMDB, this is a no-profit project for learning purposes</p>
+        <p className="mt-2 text-sm">Made with ❤️ by <a className="font-mono hover:text-blue-400 cursor-pointer" href="https://github.com/AlexanderEstebanZapata1994">Alexander Esteban</a></p>
+        <a className="font-mono hover:text-blue-400 cursor-pointer" href="https://github.com/AlexanderEstebanZapata1994/movie-portfolio-react">Link to repository</a>
       </footer>
     </div>
   )
