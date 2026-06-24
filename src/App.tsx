8ed55199ae2d20e642e0dde7ee255de666d3b8d6
Alexander Esteban Zapata <AlexanderEstebanZapata1994@users.noreply.github.com>
2026-06-24 17:53:14 -0500

    feat: Add a title and a footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import { MovieGrid } from "@/components/MovieGrid.tsx"
 function App() {
   return (
     <div className="bg-gray-500 px-5 py-10">
+      <h1 className="text-4xl text-white py-10 text-center font-mono">My Movies Portfolio</h1>
       <MovieGrid />
+      <footer className="text-center text-white py-10">
+        <p>Powered by TMDB, this is a no-profit project for learning purposes</p>
+      </footer>
     </div>
   )
 }
