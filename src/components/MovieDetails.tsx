55a00f70d2fc7eaa302a90467848a1d4eb0d52e5
Alexander Esteban Zapata <AlexanderEstebanZapata1994@users.noreply.github.com>
2026-06-30 17:44:44 -0500

    feat(style): Fixing some css styles in movie details component

--- src/components/MovieDetails.tsx
+++ src/components/MovieDetails.tsx
@@ -38,20 +38,20 @@ export const MovieDetails = () => {
             className="size-full flex flex-col justify-around items-center bg-center bg-no-repeat"
             style={{ backgroundImage: movie?.poster_path ? `url(${baseImageUrl}${movie.poster_path})` : undefined }}
         >
-            <article className="w-dvw flex flex-col justify-center items-center backdrop-blur-[250px] py-8 px-8">
+            <article className="w-dvw flex flex-col justify-center items-center backdrop-blur-[500px] py-8 px-8">
                 <h1 className="text-5xl font-bold text-center">{movie?.title}</h1>
                 <img className="xs:h-1/3 h-1/2 w-auto mt-12 mb-12 rounded-3xl" src={`${baseImageUrl}${movie?.poster_path}`} alt={movie?.title} style={{ viewTransitionName: `poster-${id}` }} />
                 <h3 className="text-3xl mb-8 font-extrabold">Overview</h3>
                 <p className="max-w-3xl text-xl font-sans leading-relaxed">{movie?.overview}</p>
                 <section>
-                    <ul className="grid grid-flow-col auto-rows-auto gap-3 mt-5">
+                    <ul className="flex flex-row flex-wrap gap-1 max-w-3xl mt-5">
                         {movie?.genres?.map((genre) => (
-                            <li className="min-w-min text-sm border bg-black/50 text-white rounded-full px-4 py-2 transition-colors hover:bg-black/70" key={genre.id}>{genre.name}</li>
+                            <li className="w-fit text-sm border text-center bg-black/50 text-white rounded-full px-4 py-2 transition-colors hover:bg-black/70" key={genre.id}>{genre.name}</li>
                         ))}
                     </ul>
                 </section>
-                <div className="flex flex-col items-center w-full p-2 mt-5">
-                    <Link className="text-md border bg-amber-700/50 text-white rounded-full px-4 py-2 transition-colors hover:bg-black/70" to="/">Home</Link>
+                <div className="flex flex-col items-center w-full p-2 mt-5 backdrop-blur-[1px]">
+                    <Link className="text-md border-2 shadow text-center border-white text-white rounded-full px-4 py-2 transition-colors hover:bg-black/70" to="/">Home</Link>
                 </div>
             </article>
         </div>
